b3fca8867b424ac439f2fcb8257b754b11881c60
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-06-04 20:57:44 +0900

    :lipstick: update color variables for sky theme

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,19 +8,32 @@ export const signUp = async (email: string, password: string) => {
 };
 
 export const login = async (email: string, password: string) => {
-  return await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error)
+      throw new Error(
+        "이메일 인증이 완료되지 않았거나, 이메일 또는 비밀번호가 올바르지 않습니다.",
+      );
+
+    return { data };
+  } catch (err) {
+    return { error: (err as Error).message };
+  }
 };
 
 export const googleLogin = async () => {
-  return await supabase.auth.signInWithOAuth({
+  const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
       redirectTo: `${window.location.origin}/calendar`,
     },
   });
+
+  if (error) throw new Error(error.message);
 };
 
 export const logout = async (): Promise<{ error: Error | null }> => {
