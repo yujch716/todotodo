46a8a26615fab5c6823f439655ea2244250cc148
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-06-07 01:45:06 +0900

    :lipstick: 전체 레이아웃 스타일 변경

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,19 +1,7 @@
 import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/pages/home/sidebar/AppSidebar.tsx";
-import { Separator } from "@radix-ui/react-separator";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb.tsx";
+import SiteHeader from "@/pages/home/siteHeader.tsx";
 
 const Layout = () => {
   const navigate = useNavigate();
@@ -25,31 +13,15 @@ const Layout = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <SidebarProvider>
+    <div className="flex h-screen w-screen bg-sky-100">
+      <div className="absolute inset-0 bg-sky-100" />
+      <SidebarProvider className="h-full w-full">
         <AppSidebar selectedId={selectedId} onSelect={handleSelectChecklist} />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-6">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">Application</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Checklist</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
+          <header>
+            <SiteHeader />
           </header>
-          <main className="px-6">
+          <main className="py-6 px-4 gap-4">
             <Outlet />
           </main>
         </SidebarInset>
