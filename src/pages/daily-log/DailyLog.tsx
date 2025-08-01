590dfbd7b754ec1b8d8d130065763aa6efc63cda
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-08-02 02:21:35 +0900

    :lipstick: daily ui apply tabs

--- src/pages/daily-log/DailyLog.tsx
+++ src/pages/daily-log/DailyLog.tsx
@@ -7,12 +7,15 @@ import { Trash2 } from "lucide-react";
 import { useDailyLogSidebarStore } from "@/store/dailyLogSidebarStore.ts";
 import EmptyDailyLog from "@/pages/daily-log/EmptyDailyLog.tsx";
 import AlertConfirmModal from "@/components/AlertConfirmModal.tsx";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs.tsx";
 
 const DailyLog = () => {
   const navigate = useNavigate();
 
   const [searchParams] = useSearchParams();
   const dailyLogId = searchParams.get("id");
+
+  const [isSmall, setIsSmall] = useState(false);
 
   const [date, setDate] = useState<Date | null>(null);
   const [memo, setMemo] = useState("");
@@ -52,6 +55,19 @@ const DailyLog = () => {
     navigate("/calendar");
   };
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1023px)");
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsSmall(e.matches);
+    };
+
+    setIsSmall(mediaQuery.matches); // 초기값
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
   if (!dailyLogId) return <EmptyDailyLog />;
 
   return (
@@ -75,14 +91,42 @@ const DailyLog = () => {
         </header>
 
         <div className="flex flex-grow overflow-hidden gap-8">
-          <div className="w-1/2 h-full flex flex-col overflow-auto">
-            <DailyTodoPanel dailyLogId={dailyLogId} />
-          </div>
-          <div className="w-1/2 h-full flex flex-col overflow-auto">
-            <MemoPanel dailyLogId={dailyLogId} memo={memo} setMemo={setMemo} />
-          </div>
+          {isSmall ? (
+            <>
+              <Tabs defaultValue="todo" className="flex flex-col w-full h-full">
+                <TabsList className="flex w-fit">
+                  <TabsTrigger value="todo">To do</TabsTrigger>
+                  <TabsTrigger value="memo">Memo</TabsTrigger>
+                </TabsList>
+                <TabsContent value="todo">
+                  <DailyTodoPanel dailyLogId={dailyLogId} />
+                </TabsContent>
+                <TabsContent value="memo" className="flex-grow">
+                  <MemoPanel
+                    dailyLogId={dailyLogId}
+                    memo={memo}
+                    setMemo={setMemo}
+                  />
+                </TabsContent>
+              </Tabs>
+            </>
+          ) : (
+            <>
+              <div className="w-1/2 h-full flex flex-col overflow-auto">
+                <DailyTodoPanel dailyLogId={dailyLogId} />
+              </div>
+              <div className="w-1/2 h-full flex flex-col overflow-auto">
+                <MemoPanel
+                  dailyLogId={dailyLogId}
+                  memo={memo}
+                  setMemo={setMemo}
+                />
+              </div>
+            </>
+          )}
         </div>
       </div>
+
       <AlertConfirmModal
         open={isAlertOpen}
         message="이 체크리스트를 삭제하시겠습니까?"
