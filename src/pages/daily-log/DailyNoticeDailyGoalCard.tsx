87a20a379a0e9ef73ebe6411a23a3cb66e5fc0e4
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-12-21 02:26:23 +0900

    :lipstick: 설정 사이드바 구성, 레이아웃 변경

--- src/pages/daily-log/DailyNoticeDailyGoalCard.tsx
+++ src/pages/daily-log/DailyNoticeDailyGoalCard.tsx
@@ -27,40 +27,40 @@ const DailyNoticeDailyGoalCard = ({
   // };
 
   return (
-      <Card
-        key={id}
-        className={`p-3 w-full cursor-pointer shadow-sm min-w-0 ${
-          isCompleted
-            ? "opacity-50"
-            : "hover:shadow-md hover:bg-gradient-to-br hover:from-white hover:to-slate-100"
-        }`}
-        // onClick={() => {
-        //   handelMoveGoal();
-        // }}
-      >
-        <div className="flex flex-row items-center gap-2 pb-2 min-w-0">
-          <span className="flex-shrink-0">{emoji}</span>
-          <h1 className="text-base font-bold leading-tight truncate min-w-0 flex-1">
-            {title}
-          </h1>
+    <Card
+      key={id}
+      className={`p-3 w-full cursor-pointer shadow-sm min-w-0 ${
+        isCompleted
+          ? "opacity-50"
+          : "hover:shadow-md hover:bg-gradient-to-br hover:from-white hover:to-slate-100"
+      }`}
+      // onClick={() => {
+      //   handelMoveGoal();
+      // }}
+    >
+      <div className="flex flex-row items-center gap-2 pb-2 min-w-0">
+        <span className="flex-shrink-0">{emoji}</span>
+        <h1 className="text-base font-bold leading-tight truncate min-w-0 flex-1">
+          {title}
+        </h1>
 
-          {isCompleted && (
-            <DailyLogStatusIcon
-              checkedCount={1}
-              totalCount={1}
-              iconClassName="w-5 h-5"
-            />
-          )}
-        </div>
+        {isCompleted && (
+          <DailyLogStatusIcon
+            checkedCount={1}
+            totalCount={1}
+            iconClassName="w-5 h-5"
+          />
+        )}
+      </div>
 
-        <div className="flex items-center gap-2 min-w-0 text-sm w-full">
-          <CalendarIcon className="w-4 h-4 text-muted-foreground flex-shrink-0" />
-          <span className="truncate">
-            {start_date ? format(start_date, "yy.MM.dd") : "시작일"} -{" "}
-            {end_date ? format(end_date, "yy.MM.dd") : "종료일"}
-          </span>
-        </div>
-      </Card>
+      <div className="flex items-center gap-2 min-w-0 text-sm w-full">
+        <CalendarIcon className="w-4 h-4 text-muted-foreground flex-shrink-0" />
+        <span className="truncate">
+          {start_date ? format(start_date, "yy.MM.dd") : "시작일"} -{" "}
+          {end_date ? format(end_date, "yy.MM.dd") : "종료일"}
+        </span>
+      </div>
+    </Card>
   );
 };
 export default DailyNoticeDailyGoalCard;
