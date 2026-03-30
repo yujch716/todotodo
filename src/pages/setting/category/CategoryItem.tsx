37df2e2f1e9c0b0b8d94d978afb69efc9128c982
yujch716 <70509403+yujch716@users.noreply.github.com>
2026-03-30 16:32:45 +0900

    :bug: 컬러피커 수정

--- src/pages/setting/category/CategoryItem.tsx
+++ src/pages/setting/category/CategoryItem.tsx
@@ -13,6 +13,7 @@ import { Input } from "@/components/ui/input.tsx";
 import { deleteCategory, updateCategory } from "@/api/category.ts";
 import { useState } from "react";
 import { useCategoryStore } from "@/store/categoryStore.ts";
+import AlertConfirmModal from "@/components/AlertConfirmModal.tsx";
 
 interface Props {
   category: Category;
@@ -22,6 +23,9 @@ const CategoryItem = ({ category }: Props) => {
   const [editingId, setEditingId] = useState<string | null>(null);
   const [editName, setEditName] = useState("");
   const [editColor, setEditColor] = useState("#fecaca");
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
+  const categoryId = category.id;
 
   const triggerCalendarCategoryRefresh = useCategoryStore(
     (state) => state.triggerCategoryRefresh,
@@ -40,8 +44,16 @@ const CategoryItem = ({ category }: Props) => {
     triggerCalendarCategoryRefresh();
   };
 
-  const handleDelete = async (id: string) => {
-    await deleteCategory(id);
+  const handleDelete = async () => {
+    setIsAlertOpen(true);
+  };
+
+  const handleDeleteCategory = async () => {
+    if (!categoryId) return;
+
+    setIsAlertOpen(false);
+
+    await deleteCategory(categoryId);
 
     triggerCalendarCategoryRefresh();
   };
@@ -53,74 +65,83 @@ const CategoryItem = ({ category }: Props) => {
   };
 
   return (
-    <Item
-      variant="outline"
-      role="listitem"
-      className="flex items-center hover:bg-gradient-to-br hover:from-white hover:to-slate-100 "
-    >
-      {editingId === category.id ? (
-        <>
-          <ItemMedia>
-            <ColorPicker value={editColor} onChange={setEditColor} />
-          </ItemMedia>
-          <ItemContent>
-            <ItemTitle>
-              <Input
-                value={editName}
-                onChange={(e) => setEditName(e.target.value)}
-                className="flex-1"
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    e.preventDefault();
-                    handleUpdate();
-                  }
-                }}
+    <>
+      <Item
+        variant="outline"
+        role="listitem"
+        className="flex items-center hover:bg-gradient-to-br hover:from-white hover:to-slate-100 "
+      >
+        {editingId === category.id ? (
+          <>
+            <ItemMedia>
+              <ColorPicker value={editColor} onChange={setEditColor} />
+            </ItemMedia>
+            <ItemContent>
+              <ItemTitle>
+                <Input
+                  value={editName}
+                  onChange={(e) => setEditName(e.target.value)}
+                  className="flex-1"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.preventDefault();
+                      handleUpdate();
+                    }
+                  }}
+                />
+              </ItemTitle>
+            </ItemContent>
+            <ItemActions>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="border border-transparent hover:border-slate-300"
+                onClick={handleUpdate}
+              >
+                <Save className="w-4 h-4" />
+              </Button>
+            </ItemActions>
+          </>
+        ) : (
+          <>
+            <ItemMedia>
+              <div
+                className="w-6 h-6 rounded-full"
+                style={{ backgroundColor: category.color }}
               />
-            </ItemTitle>
-          </ItemContent>
-          <ItemActions>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="border border-transparent hover:border-slate-300"
-              onClick={handleUpdate}
-            >
-              <Save className="w-4 h-4" />
-            </Button>
-          </ItemActions>
-        </>
-      ) : (
-        <>
-          <ItemMedia>
-            <div
-              className="w-6 h-6 rounded-full"
-              style={{ backgroundColor: category.color }}
-            />
-          </ItemMedia>
-          <ItemContent>
-            <ItemTitle>{category.name}</ItemTitle>
-          </ItemContent>
-          <ItemActions>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="border border-transparent hover:border-slate-300"
-              onClick={() => startEditing(category)}
-            >
-              <Pencil className="w-4 h-4 cursor-pointer" />
-            </Button>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="border border-transparent hover:border-slate-300"
-              onClick={() => handleDelete(category.id)}
-            >
-              <X className="w-4 h-4" />
-            </Button>
-          </ItemActions>
-        </>
-      )}
-    </Item>
+            </ItemMedia>
+            <ItemContent>
+              <ItemTitle>{category.name}</ItemTitle>
+            </ItemContent>
+            <ItemActions>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="border border-transparent hover:border-slate-300"
+                onClick={() => startEditing(category)}
+              >
+                <Pencil className="w-4 h-4 cursor-pointer" />
+              </Button>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="border border-transparent hover:border-slate-300"
+                onClick={() => handleDelete()}
+              >
+                <X className="w-4 h-4" />
+              </Button>
+            </ItemActions>
+          </>
+        )}
+      </Item>
+
+      <AlertConfirmModal
+        open={isAlertOpen}
+        message="이 카테고리를 삭제하시겠습니까?"
+        onConfirm={handleDeleteCategory}
+        onCancel={() => setIsAlertOpen(false)}
+      />
+    </>
   );
 };
 export default CategoryItem;
